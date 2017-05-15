6aacc0f7c2ade768950ca5bac19f5ae509b8ee21
Danielle Fricke <danielle.fricke11@gmail.com>
2017-05-15 10:52:24 -0400

    created question object

---
+++ script.js
@@ -0,0 +1,38 @@
+/*facts about dc
+Which 2 presidents are buried at Arlington? Kennedy & Taft
+DC's official bird: Wood Thrush
+What house, formerly owned by a member of the Lincoln Cabinet, is now the guesthouse of the executive branch? The Blair house
+What festival attracts over a million visitors to DC every year: Cherry Blossom festival
+How large is the landmass of DC? 100sq miles
+Official Flower: American Beauty Rose
+What river runs by washington DC? Potomac
+What is the name of the statue on top of the capitol building? Freedom
+DC is between which two states? MD and VA
+When was Georgetown incorporated into DC? 1871
+DC's football team: Redskins
+How many daVinvi's are at the national art gallery? 1
+Who attacked and burned the white house and in what year? British 1914
+First president to live in the white house? John Adams
+Tree of DC? Scarlett Oak
+Motto of DC? Justitia Omnibus
+What was the capital called before it was called DC? Federal City
+
+Set these up as an object/class with 2 keys (question & true or false).
+Create a function that loops through the questions one by owne
+Envoke a function that takes the input and evaluates it aganst the answer
+Add points when player is correct
+Prompt a total after 10 questions
+*/
+
+let questions = [
+  {que: 'Kennedy and Taft are the only two presidents buried at Arlington National Cemetary', answer: 'true'},
+  {que: 'The Liberty Rose is the official flower of DC', answer: 'false'},
+  {que: 'Georgetown was incorporated into DC in 1871', answer: 'true'},
+  {que: 'DCs pro football team is the Redskins', answer: 'true'},
+  {que: 'Thomas Jefferson was the first president to live in the White House', answer: 'false'},
+  {que: 'DCs landmass is 100 square miles', answer: 'true'},
+  {que: 'The official tree of DC is the Scarlett Oak', answer: 'true'},
+  {que: 'The official motto of DC is Ab Ordine Libertas - from order comes freedom', answer: 'false'},
+  {que: 'Before being called The District of Columbia, DC was called Federal City', answer; 'true'},
+  {que: 'The official bird of DC is the Bald Eagle', answer:'false'}
+]
