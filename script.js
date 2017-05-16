92fc38ff5996a6c02be5be46d35c7a3c66b948ad
Danielle Fricke <danielle.fricke11@gmail.com>
2017-05-16 15:46:04 -0400

    game is going, not evaluating .val correctly'

--- script.js
+++ script.js
@@ -7,44 +7,46 @@ Prompt a total after 10 questions
 $(document).ready(function () {
 
   var clickCounter = 0
+  var intClickCounter = -1
 
   let questions = [
-    {que: 'Kennedy and Taft are the only two presidents buried at Arlington National Cemetary', answer: 'true', backup: 'This is true!'},
-    {que: 'The Liberty Rose is the official flower of DC', answer: 'false', backup: 'This is false. The official flower is the American Beauty Rose'},
-    {que: 'Georgetown was incorporated into DC in 1871', answer: 'true', backup: 'This is true!'},
-    {que: 'DCs pro football team is the Redskins', answer: 'true', backup: 'This is true!'},
-    {que: 'Thomas Jefferson was the first president to live in the White House', answer: 'false', backup: 'This is false. John Adams was the first president to live in the White House'},
-    {que: 'DCs landmass is 100 square miles', answer: 'true', backup: 'This is true!'},
-    {que: 'The official tree of DC is the Scarlett Oak', answer: 'true', backup: 'This is true!'},
-    {que: 'The official motto of DC is Ab Ordine Libertas - from order comes freedom', answer: 'false', backup: 'This is false! The official DC motto is Justitia Omnibus'},
-    {que: 'Before being called The District of Columbia, DC was called Federal City', answer: 'true', backup: 'This is true!'},
-    {que: 'The official bird of DC is the Bald Eagle', answer:'false', backup: 'This is false. The official bird is the Wood Thrush'}
+    {que: 'Kennedy and Taft are the only two presidents buried at Arlington National Cemetary', answer: "true", backup: 'This is true!'},
+    {que: 'The Liberty Rose is the official flower of DC', answer: "false", backup: 'This is false. The official flower is the American Beauty Rose'},
+    {que: 'Georgetown was incorporated into DC in 1871', answer: "true", backup: 'This is true!'},
+    {que: 'DCs pro football team is the Redskins', answer: "true", backup: 'This is true!'},
+    {que: 'Thomas Jefferson was the first president to live in the White House', answer: "false", backup: 'This is false. John Adams was the first president to live in the White House'},
+    {que: 'DCs landmass is 100 square miles', answer: "true", backup: 'This is true!'},
+    {que: 'The official tree of DC is the Scarlett Oak', answer: "true", backup: 'This is true!'},
+    {que: 'The official motto of DC is Ab Ordine Libertas - from order comes freedom', answer: "false", backup: 'This is false! The official DC motto is Justitia Omnibus'},
+    {que: 'Before being called The District of Columbia, DC was called Federal City', answer: "true", backup: 'This is true!'},
+    {que: 'The official bird of DC is the Bald Eagle', answer:"false", backup: 'This is false. The official bird is the Wood Thrush'},
+    {que: 'Thanks for playing!!!'}
   ]
 
   var output = questions[clickCounter].que;
   var currentQuestion = [];
   $(".appfield .qwindow").text(output)
-  console.log(output)
-  console.log(questions[0].backup)
+  // console.log(output)
+  // console.log(questions[0].backup)
   $(".appfield .go").click(function() {
       clickCounter++
       output = questions[clickCounter].que;
       $(".appfield .qwindow").text(output)
-      console.log(output)
-      console.log(questions[clickCounter].backup)
+      //console.log(output)
+      //console.log(questions[clickCounter].backup)
       })
 
+
 //evaluat answer and award points
-  var userAnswer = $("appfield .input").text();
-  var balance = $("appfield .points").val();
   var currentAnswer = questions[clickCounter].answer;
   $(".appfield .go").click(function() {
-
-      if (userAnswer == "true" && currentAnswer == "true" || userAnswer == "false" && currentAnswer == "false"){
-        var givePoints = balance + 10;
-        prompt: "You are correct!"
+      clickCounter.length = 10, intClickCounter++
+  var userAnswer = $(".appfield .answerfield").val();
+      console.log("This is the user answer: " + userAnswer)
+      if (userAnswer === currentAnswer){
+        alert("You are correct!");
       } else {
-        prompt: questions[clickCounter].backup
+        alert(questions[intClickCounter].backup);
       }
   })
 })
